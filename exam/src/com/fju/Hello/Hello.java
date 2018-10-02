5d6be34015df8e4597d1ba93997534b4fd13f3e5
amanda <user@es302n21>
2018-10-02 10:57:55 +0800

    exam

---
+++ exam/src/com/fju/Hello/Hello.java
@@ -0,0 +1,19 @@
+package com.fju.Hello;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+	     String name = "Amanda" ; 
+	     int age = 18 ;
+	     int schoolYear = 107 ;
+	     long birthday = 212 ;
+	     System.out.println(name);
+	     System.out.println(age);
+	     System.out.println(schoolYear);
+	     System.out.println(birthday);
+		}
+	}
+
+}
