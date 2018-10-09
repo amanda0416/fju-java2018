913d3b6b1deb776b5024e547985534a2b5a807fa
amanda <user@es302n21>
2018-10-09 12:00:54 +0800

    10/9

--- myproject/src/com/tom/Hello.java
+++ myproject/src/com/tom/Hello.java
@@ -1,18 +1,30 @@
 package com.tom;
 
 public class Hello {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-     System.out.println("Hello world!");
+	Person p = new Person(53.5f,1.65f) ;
+  //p.weight = 53.5f ; 
+  //p.height = 1.65f ;
+    System.out.println(p.bmi());
+    p.hello();
+  //Person hank = new Person() ;
+  //Person jack = null ;
+	
+   /*System.out.println("Hello world!");
      String name = "Amanda" ; 
      int age = 18 ;
      int schoolYear = 107 ;
      long birthday = 212 ;
+     float height = 1.6f ;
+     float weight = 50.5f ;
+     float bmi = weight / (height * height);
      System.out.println(name);
      System.out.println(age);
      System.out.println(schoolYear);
      System.out.println(birthday);
+     System.out.println(height);
+     System.out.println(weight);
+     System.out.println(bmi);*/
 	}
 
 }
