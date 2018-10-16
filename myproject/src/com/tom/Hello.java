4985dd3c069785d384d7953a88f51511b6ace892
amanda <user@es302n21>
2018-10-16 11:57:04 +0800

    10/16

--- myproject/src/com/tom/Hello.java
+++ myproject/src/com/tom/Hello.java
@@ -2,11 +2,24 @@ package com.tom;
 
 public class Hello {
 	public static void main(String[] args) {
-		String name ="Hank";
-		int english = 70;
-		int math = 90;
-		System.out.println(name +"\t"+ english +"\t"+ math +"\t"+(english+math)/2);
+		Student stu = new Student("Hank", 70, 90);
+		stu.print();
 		
+		System.out.println(3 == 5);
+		System.out.println(3 <= 5);
+		String s1 = new String("abc");
+		String s2 = new String("abc");
+		System.out.println(s1 == s2);
+		System.out.println(s1.equals(s2));
+		
+		String s3 = "abc";
+		String s4 = "abc";
+		System.out.println(s3 == s4);
+
+	   /*stu.name = "Hank"; 
+		 stu.english = 70;
+		 stu.math = 90;*/
+
 		Person p = new Person(53.5f, 1.65f);
 		// p.weight = 53.5f ;
 		// p.height = 1.65f ;
@@ -15,15 +28,18 @@ public class Hello {
 		// Person hank = new Person() ;
 		// Person jack = null ;
 
-		/*
-		 * System.out.println("Hello world!"); String name = "Amanda" ; int age
-		 * = 18 ; int schoolYear = 107 ; long birthday = 212 ; float height =
-		 * 1.6f ; float weight = 50.5f ; float bmi = weight / (height * height);
-		 * System.out.println(name); System.out.println(age);
-		 * System.out.println(schoolYear); System.out.println(birthday);
-		 * System.out.println(height); System.out.println(weight);
-		 * System.out.println(bmi);
-		 */
+	   /*System.out.println("Hello world!"); 
+		 String name = "Amanda" ; 
+		 int age= 18 ;
+		 int schoolYear = 107 ; 
+		 long birthday = 212 ; 
+		 float height =1.6f ;
+		 float weight = 50.5f ; 
+		 float bmi = weight / (height * height);
+		 System.out.println(name); System.out.println(age);
+		 System.out.println(schoolYear); System.out.println(birthday);
+		 System.out.println(height); System.out.println(weight);
+		 System.out.println(bmi);*/
 	}
 
 }
