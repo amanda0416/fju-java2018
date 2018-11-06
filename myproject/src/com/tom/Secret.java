657f92e3124b2e4f8140071b1db3e687f63cfe16
amanda <user@es302n21>
2018-11-06 09:12:50 +0800

    猜數字

---
+++ myproject/src/com/tom/Secret.java
@@ -0,0 +1,34 @@
+package com.tom;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Secret {
+	public static void main(String[] args) {
+		Random random = new Random();
+		int secret = random.nextInt(100)+1;
+		System.out.println(secret);
+		System.out.print("Please write a number(1~100):");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		while (number!=secret) {
+			if (number <= secret) {
+				System.out.print(number+" to 100:");
+				int number1 = scanner.nextInt();
+				
+			}
+			if(number >= secret) {
+				System.out.print("1 to "+number+":");
+				int number2 = scanner.nextInt();
+				
+			}
+			
+		} 
+		while(number == secret) {
+			System.out.println("¦n´Î´Î!!");
+			break;
+		}
+		
+	}	
+			
+}
